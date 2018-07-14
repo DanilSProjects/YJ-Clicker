b5ad110df3525b4665b208d04e2587e98f19146a
TwoEyedMikeWazowski <danielsyrov9@gmail.com>
2018-07-14 21:02:29 +0800

    Gethacking design

---
+++ YJ Clicker/GetHackingViewController.swift
@@ -0,0 +1,35 @@
+//
+//  GetHackingViewController.swift
+//  YJ Clicker
+//
+//  Created by Daniel on 14/7/18.
+//  Copyright © 2018 Placeholder Interactive. All rights reserved.
+//
+
+import UIKit
+
+class GetHackingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
